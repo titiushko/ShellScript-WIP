11dee46666a3f1882af69d2d9b7d1a11d6dc863e
Tito Miguel <tito.javier.miguel.galdamez@gmail.com>
2015-08-09 02:07:41 -0600

    ver las variables del sistema

---
+++ environment-variables.cmd
@@ -0,0 +1,20 @@
+@echo off
+ECHO Informaciones que nos suministran las variables de entorno
+echo.
+echo S.O actual                         %OS% 
+echo Fecha actual                       %DATE% 
+echo Hora actual                        %TIME%    
+echo Nombre del PC                      %COMPUTERNAME% 
+echo Nombre del usuario                 %USERNAME% 
+echo Directorio raiz del HD             %SYSTEMROOT% 
+echo Directorio en que te encuentras    %CD%
+echo Directorio de archivos temporales  %TEMP% 
+echo Numero de procesadores             %NUMBER_OF_PROCESSORS% 
+echo Descripción del procesador         %PROCESSOR_IDENTIFIER%
+echo Arquitectura del CPU               %PROCESSOR_ARCHITECTURE% 
+echo Modelo del procesador numero       %PROCESSOR_LEVEL%  
+echo Numero revision procesador         %PROCESSOR_REVISION% 
+
+
+
+pause>nul
