9d86c03cdd8119cb0646a9cfe49ff0c2efcf85f9
Tito Miguel <tito.javier.miguel.galdamez@gmail.com>
2015-08-09 02:10:56 -0600

    limpiar conversaciones de spark

---
+++ clean-transcripts.bat
@@ -0,0 +1,7 @@
+@echo off
+mode con cols=30 lines=20
+color b
+rd C:\Users\tito.miguel\AppData\Roaming\Spark\user\tito.miguel\transcripts\ /S /Q
+rd C:\Users\tito.miguel\AppData\Roaming\Spark\tempImages\ /S /Q
+echo clean transcripts finish!
+pause>nul
