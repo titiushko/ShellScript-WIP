8d6247f4ee1a96600c4243f459c702920e9a759d
Titiushko <titotauro@gmail.com>
2015-08-11 12:07:22 -0600

    implementando comandos para el uso de la pequeña aplicación con netstat

--- netstat/network-statistics.bat
+++ netstat/network-statistics.bat
@@ -7,43 +7,52 @@ echo   PROTO  DIRECCION LOCAL        DIRECCION REMOTA       ESTADO          PID 
 cmd /c %*"netstat -tona|FINDSTR /C:LISTENING"
 :menu
 echo.
-echo 1) Ver Proceso.
-echo 2) Ver Todos los Procesos.
-echo 3) Eliminar Proceso.
-echo 4) Limpiar.
-echo 5) Salir.
-choice /c 12345 /n
-if %errorlevel% equ 5 exit
-if %errorlevel% equ 4 goto inicio
-if %errorlevel% equ 3 goto opcion_3
-if %errorlevel% equ 2 goto opcion_2
-if %errorlevel% equ 1 goto opcion_1
-:opcion_1
+echo Opciones...
+echo  -bc PID buscar un proceso por codigo, ejemplo -bc 2660.
+echo  -bn IMAGENAME buscar un proceso por nombre, ejemplo -bn chrome.
+echo  -vs Ver todos los procesos del sistema.
+echo  -vu Ver todos los procesos de los usuarios locales.
+echo  -ec PID eliminar un proceso por codigo, ejemplo -ec 2660.
+echo  -en IMAGENAME eliminar un proceso por nombre, ejemplo -en chrome.
+echo  -l Limpiar pantalla.
+echo  -q Salir.
+echo ------------------------------------------------------------------
+:comando
+set comando=
+set opcion=
+set proceso=
+set /p comando=# 
+if not defined comando goto no_es_comando
+set opcion=%comando:~0,3%
+set proceso=%comando:~4,10%
+if /i %opcion%==-q exit
+if /i %opcion%==-bc goto opcion_1
+if /i %opcion%==-bn goto opcion_2
+if /i %opcion%==-vs goto opcion_3
+if /i %opcion%==-vu goto opcion_4
+if /i %opcion%==-ec goto opcion_5
+if /i %opcion%==-en goto opcion_6
+if /i %opcion%==-l goto inicio
+:no_es_comando
 echo.
-set /p pid=ID del proceso: 
-if not defined pid goto opcion_1
-tasklist /svc /fi "pid eq %pid%"
-set pid=
+goto comando
+:opcion_1
+tasklist /v /fi "PID eq %proceso%"
 goto menu
 :opcion_2
-echo.
-echo 1) Sistema.
-echo 2) Usuarios.
-choice /c 12 /n
-if %errorlevel% equ 2 goto procesos_usuarios
-if %errorlevel% equ 1 goto procesos_sistema
-:procesos_usuarios
-tasklist /v /fi "USERNAME ne NT AUTHORITY\SYSTEM" /fi "USERNAME ne NT AUTHORITY\SERVICIO LOCAL" /fi "USERNAME ne NT AUTHORITY\Servicio de red"
+tasklist /v /fi "IMAGENAME eq %proceso%.exe"
 goto menu
-:procesos_sistema
+:opcion_3
 tasklist /v /fi "USERNAME eq NT AUTHORITY\SYSTEM"
 tasklist /v /fi "USERNAME eq NT AUTHORITY\SERVICIO LOCAL"
 tasklist /v /fi "USERNAME eq NT AUTHORITY\Servicio de red"
 goto menu
-:opcion_3
-echo.
-set /p proceso=Nombre del proceso: 
-if not defined proceso goto opcion_2
+:opcion_4
+tasklist /v /fi "USERNAME ne NT AUTHORITY\SYSTEM" /fi "USERNAME ne NT AUTHORITY\SERVICIO LOCAL" /fi "USERNAME ne NT AUTHORITY\Servicio de red"
+goto menu
+:opcion_5
+taskkill /f /pid %proceso%
+goto menu
+:opcion_6
 taskkill /f /im %proceso%.exe
-set proceso=
 goto menu
