3f3fb9f80ec984e894c8be95b39ee84e49238488
Titiushko <titotauro@gmail.com>
2015-08-11 12:16:15 -0600

    reiniciar o apagar el sistema y eliminar procesos forzosamente

--- shut-down.bat
+++ shut-down.bat
@@ -9,4 +9,7 @@ if %opcion%==R goto continuar
 if %opcion%==r goto continuar
 goto inicio
 :continuar
+for /f %%f in ('tasklist /fi "USERNAME ne NT AUTHORITY\SYSTEM" /fi "USERNAME ne NT AUTHORITY\SERVICIO LOCAL" /fi "USERNAME ne NT AUTHORITY\Servicio de red" /fi "IMAGENAME ne System" /fi "IMAGENAME ne explorer.exe" /fi "IMAGENAME ne dllhost.exe" /fi "IMAGENAME ne dwm.exe" /fi "IMAGENAME ne cmd.exe" /fi "IMAGENAME ne tasklist.exe" /fi "IMAGENAME ne conhost.exe" /fi "IMAGENAME ne bash.exe"') do (
+	if /i not %%f==nombre if /i not %%f=========================== taskkill /f /im %%f
+)
 cmd /c %*"shutdown -%opcion% -f -t 0"
